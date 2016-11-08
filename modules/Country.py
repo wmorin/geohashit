8264cd6a29645b71eccbb0430d559d57d10c7370
Willy Morin <willy.morin@blablacar.com>
2016-11-08 10:41:47 +0100

    Add country module

---
+++ modules/Country.py
@@ -0,0 +1,15 @@
+class Country:
+    def set_place_id(self, id):
+        self.place_id = id
+
+    def set_centroid(self, lat, lon):
+        self.centroid = {
+            'lat': lat,
+            'lon': lon,
+        }
+
+    def set_geometry(self, geometry):
+        self.geometry = geometry
+
+    def get_geometry(self):
+        return self.geometry
